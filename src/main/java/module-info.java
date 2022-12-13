adbc7b3a2db9498dea15c7a7c603c17bd62bbc89
Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2022-12-13 11:23:22 +0100

    SetUpGame

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module ws2022 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
-    opens ws2022 to javafx.fxml;
+    opens ws2022;
+
     exports ws2022;
 }
--- src/main/java/ws2022/Client/Logic/SetUpGame.java
+++ src/main/java/ws2022/Client/Logic/SetUpGame.java
@@ -1,5 +1,27 @@
 package ws2022.Client.Logic;
 
-public class SetUpGame {
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SetUpGame extends Application {
+    public void start(Stage stage) throws IOException {
+        URL url = new File("../ViewFx/boardgame.fxml")
+                .toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        stage.setTitle("Nanu");
+        stage.setScene(new Scene(root));
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
