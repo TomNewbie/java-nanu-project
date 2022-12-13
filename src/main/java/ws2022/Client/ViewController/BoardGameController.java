db24ab8ab7456476fc2ace2ce43d6099eb6c2dad
Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2022-12-13 23:12:35 +0100

    Add eventListener to disc

--- src/main/java/ws2022/Client/ViewController/BoardGameController.java
+++ src/main/java/ws2022/Client/ViewController/BoardGameController.java
@@ -1,9 +1,14 @@
 package ws2022.Client.ViewController;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import ws2022.Client.Model.Disc;
+
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,6 +17,7 @@ import java.util.Collections;
 public class BoardGameController {
     @FXML
     public GridPane boardgame;
+    public Button myButton;
 
     @FXML
     public void initialize() throws FileNotFoundException {
@@ -22,12 +28,15 @@ public class BoardGameController {
                 "Mexico", "Netherland", "Poland", "Portugal", "Qatar", "SaudiArabia",
                 "Senegal", "Serbia", "Spain",
                 "Switzerland", "Tunisia", "Uruguay", "USA", "Wales" };
+        Disc disc1 = new Disc("Argentina.png", "Argentina");
+        Disc disc2 = new Disc("Australia.png", "Australia");
 
         ArrayList<String> myList = new ArrayList<>(Arrays.asList(arrFlag));
         Collections.shuffle(myList);
         int index = 0;
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 4; j++) {
+
                 String selectedImage = "/ws2022/Client/assets/FootballTheme/" + myList.get(index) + ".png";
                 Image image = new Image(
                         this.getClass()
@@ -36,11 +45,34 @@ public class BoardGameController {
                 ImageView imageView = new ImageView(image);
                 imageView.setFitWidth(120);
                 imageView.setFitHeight(120);
+                imageView.setUserData(disc1); // change
+                imageView.setOnMouseClicked(event -> showDisc(event));
                 boardgame.add(imageView, i, j);
                 index++;
             }
         }
 
+    }
+    // random cover
+
+    // change first image to USA when click remeber all
+    @FXML
+    public void changeImage() {
+        Image image = new Image(
+                this.getClass()
+                        .getResourceAsStream("/ws2022/Client/assets/FootballTheme/USA.png"));
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(120);
+        imageView.setFitHeight(120);
+        boardgame.add(imageView, 0, 0);
+    }
+
+    // show disc value when click on disc
+    @FXML
+    public void showDisc(MouseEvent event) {
+        Node sourceComponent = (Node) event.getSource();
+        Disc disc = (Disc) sourceComponent.getUserData();
+        System.out.println(disc.getValue() + " and " + disc.getImage());
     }
 
 }
--- src/main/java/ws2022/client/Model/Disc.java
+++ src/main/java/ws2022/client/Model/Disc.java
@@ -3,14 +3,27 @@ package ws2022.Client.Model;
 public class Disc {
     private String cardImage;
     private String value;
+    private String anotherImage;
 
-    public String getValue(Disc disc) {
-        return disc.value;
+    public String getValue() {
+        return value;
     };
+
+    public String getImage() {
+        if (anotherImage == null) {
+            return cardImage;
+        } else {
+            return anotherImage;
+        }
+    }
 
     public Disc(String cardImage, String value) {
         this.cardImage = cardImage;
         this.value = value;
+    }
+
+    public void setAnotherImage(String anotherImage) {
+        this.anotherImage = anotherImage;
     }
 
 }
