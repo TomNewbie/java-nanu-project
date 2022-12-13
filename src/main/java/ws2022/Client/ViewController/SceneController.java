162af118938240b1e2d435396c219ee84d6bb081
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2022-12-13 10:57:47 +0100

    add folder resource maven

--- src/main/java/ws2022/Client/ViewController/SceneController.java
+++ src/main/java/ws2022/Client/ViewController/SceneController.java
@@ -1,8 +1,12 @@
 package ws2022.Client.ViewController;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -14,12 +18,20 @@ public class SceneController {
     private Parent root;
 
     @FXML
-    // private void switchToPrimary() throws IOException {
-    // root = FXMLLoader.load(getClass().getResource("../ViewFx/primary.fxml"));
-    // stage = (Stage)((Node)event.getResource()).getScene().getWindow();
-    // scene = new
-    // }
+    public void switchToPrimary(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../ViewFx/primary.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
-    public void switchToSecondary() {
+    @FXML
+    public void switchToSecondary(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../ViewFx/secondary.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
