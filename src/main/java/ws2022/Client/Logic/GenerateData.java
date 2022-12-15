a9579cb7cf0e9e63b3972e52b315944698275bc8
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2022-12-15 16:45:11 +0100

    add someInstruction

--- src/main/java/ws2022/Client/Logic/GenerateData.java
+++ src/main/java/ws2022/Client/Logic/GenerateData.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 import ws2022.Client.Model.Disc;
 
 public class GenerateData {
+    // Add value flag ngay tren array nay
     private String[] flagValue;
+    // Add name file flag tuong ung tren array nay trong ws2022/Client/assets
     private String[] flagFileName;
 
     public void generateDisc(ArrayList<Disc> discArray) {
