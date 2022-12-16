a53e035d73220e3655295ee661c91a91deebb54f
PhamNhuNgoc <100960531+PhamNhuNgoc@users.noreply.github.com>
2022-12-16 18:50:34 +0700

    Update GenerateData.java

--- src/main/java/ws2022/Client/Logic/GenerateData.java
+++ src/main/java/ws2022/Client/Logic/GenerateData.java
@@ -7,9 +7,70 @@ import ws2022.Client.Model.Disc;
 public class GenerateData {
     // Add value flag ngay tren array nay
     private String[] flagValue;
+    flagValue[0]="Argentina";
+    flagValue[1]="Australia";
+    flagValue[2]="Belgium";
+    flagValue[3]="Brazil";
+    flagValue[4]="Cameroon";
+    flagValue[5]="Canada";
+    flagValue[6]="Costa Rica";
+    flagValue[7]="Croatian";
+    flagValue[8]="Denmark";
+    flagValue[9]="Ecuador";
+    flagValue[10]="England";
+    flagValue[11]="France";
+    flagValue[12]="Germany";
+    flagValue[13]="Ghana";
+    flagValue[14]="Iran";
+    flagValue[15]="Japan";
+    flagValue[16]="Korea";
+    flagValue[17]="Mexico";
+    flagValue[18]="Netherland";
+    flagValue[19]="Poland";
+    flagValue[20]="Portugal";
+    flagValue[21]="Qatar";
+    flagValue[22]="Saudi Arabia";
+    flagValue[23]="Senegal";
+    flagValue[24]="Serbia";
+    flagValue[25]="Spain";
+    flagValue[26]="Switzerland";
+    flagValue[27]="Tunisia";
+    flagValue[28]="Uruguay";
+    flagValue[29]="USA";
+    flagValue[30]="Wales";
     // Add name file flag tuong ung tren array nay trong ws2022/Client/assets
     private String[] flagFileName;
-
+    flagFileName[0]="Argentina.png";
+    flagFileName[1]="Australia.png";
+    flagFileName[2]="Belgium.png";
+    flagFileName[3]="Brazil.png";
+    flagFileName[4]="Cameroon.png";
+    flagFileName[5]="Canada.png";
+    flagFileName[6]="CostaRica.png";
+    flagFileName[7]="Croatia.png";
+    flagFileName[8]="Denmark.png";
+    flagFileName[9]="Ecuador.png";
+    flagFileName[10]="England.png";
+    flagFileName[11]="France.png";
+    flagFileName[12]="Germany.png";
+    flagFileName[13]="ghana.png";
+    flagFileName[14]="Iran.png";
+    flagFileName[15]="Japan.png";
+    flagFileName[16]="Korea.png";
+    flagFileName[17]="Mexico.png";
+    flagFileName[18]="Netherland.png";
+    flagFileName[19]="Poland.png";
+    flagFileName[20]="Portugal.png";
+    flagFileName[21]="Qatar.png";
+    flagFileName[22]="SaudiaArabia.png";
+    flagFileName[23]="Senegal.png";
+    flagFileName[24]="Serbia";
+    flagFileName[25]="Spain";
+    flagFileName[26]="Switzerland.png";
+    flagFileName[27]="Tunisia";
+    flagFileName[28]="Uruguay.png";
+    flagFileName[29]="USA.png";
+    flagFileName[30]="Wales.png";
     public void generateDisc(ArrayList<Disc> discArray) {
 
     }
