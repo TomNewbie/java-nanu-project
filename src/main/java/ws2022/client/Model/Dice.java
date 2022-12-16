87ad528b8fcdca9b5ee8464e3d95f279c1cd6e9f
Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2022-12-16 14:12:49 +0100

    Add guess picture button

--- src/main/java/ws2022/client/Model/Dice.java
+++ src/main/java/ws2022/client/Model/Dice.java
@@ -8,8 +8,33 @@ public class Dice {
     public Dice() {
     };
 
-    public int getDiceValues() {
-        return this.color;
+    public String getDiceValues() {
+        String color;
+        switch (this.color) {
+            case 0:
+                color = "red";
+                break;
+            case 1:
+                color = "green";
+                break;
+            case 2:
+                color = "blue";
+                break;
+            case 3:
+                color = "yellow";
+                break;
+            case 4:
+                color = "orange";
+                break;
+            case 5:
+                color = "joker";
+                break;
+            default:
+                color = "";
+                break;
+
+        }
+        return color;
     }
 
     public void rollDice() {
