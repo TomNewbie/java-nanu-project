a6870c68a074810fc246f2c7e50ae8aa1f1156fa
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2022-12-21 16:27:35 +0100

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,8 @@ module ws2022 {
     exports ws2022.Client.Model;
     exports ws2022.Client.RunScreen;
     exports ws2022.Client.ViewController;
+    exports ws2022.Client.Model;
     exports ws2022.Client.Logic;
     exports ws2022;
+
 }
