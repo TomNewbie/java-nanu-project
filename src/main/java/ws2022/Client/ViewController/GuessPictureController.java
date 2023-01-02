0fbea247949f0a2e80adef9251d8fed519e15f96
Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2023-01-02 01:01:51 +0100

    Delete comment

--- src/main/java/ws2022/Client/ViewController/GuessPictureController.java
+++ src/main/java/ws2022/Client/ViewController/GuessPictureController.java
@@ -1,24 +1,4 @@
-// package ws2022.Client.ViewController;
 
-// import javafx.fxml.FXML;
-// import javafx.scene.control.ChoiceBox;
-// import javafx.scene.text.Text;
-// import ws2022.Client.Model.Disc;
-// import ws2022.Client.Model.GameManager;
-
-// public class GuessPictureController {
-//     @FXML
-//     private Text cover;
-
-//     @FXML
-//     public ChoiceBox<Disc> choiceBox;
-
-//     public void initialize() {
-//         choiceBox.getItems().addAll(GameManager.myList);
-//         String color;
-//         cover.setText(color + " cover ?");
-//     }
-// }
 package ws2022.Client.ViewController;
 
 import java.io.IOException;
@@ -43,22 +23,7 @@ public class GuessPictureController {
 
     @FXML
     private ChoiceBox<String> choicebox = new ChoiceBox<>();
-    // public void clickGuessPicture(ActionEvent event, String color) throws
-    // IOException {
-    // Stage popupwindow = new Stage();
 
-    // popupwindow.initModality(Modality.APPLICATION_MODAL);
-    // popupwindow.setTitle("This is a pop up window");
-
-    // Parent popUp = FXMLLoader
-    // .load(PopUpController.class.getResource("/ws2022/Client/ViewFx/guessPicture.fxml"));
-    // // choiceBox.getItems().addAll(GameManager.myList);
-    // cover.setText(color + " cover ?");
-    // Scene scene = new Scene(popUp);
-    // popupwindow.setScene(scene);
-
-    // popupwindow.showAndWait();
-    // }
     public void display(String color) {
         cover.setText(color + " cover ?");
         ArrayList<String> answer = new ArrayList();
