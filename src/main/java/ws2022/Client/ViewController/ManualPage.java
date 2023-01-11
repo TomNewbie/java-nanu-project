5ba0df77670b72ec1c46529c1954d8a113a365f4
Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2023-01-11 11:38:28 +0100

    Update css

--- src/main/java/ws2022/Client/ViewController/ManualPage.java
+++ src/main/java/ws2022/Client/ViewController/ManualPage.java
@@ -12,13 +12,16 @@ import javafx.stage.Stage;
 public class ManualPage {
    private Stage stage;
    private Scene scene;
-   private Parent root; 
-   
+   private Parent root;
+
    public void gotohomescreen(ActionEvent event) throws IOException {
-	   Parent root = FXMLLoader.load(getClass().getResource("/ws2022/Client/ViewFx/HomeScreen.fxml"));
-	   stage=(Stage)((Node)event.getSource()).getScene().getWindow();
-	   scene = new Scene(root);
-	   stage.setScene(scene);
-	   stage.show();
+      Parent root = FXMLLoader.load(getClass().getResource("/ws2022/Client/ViewFx/HomeScreen.fxml"));
+      stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+      scene = new Scene(root);
+      scene.getStylesheets()
+            .add(getClass().getResource("/ws2022/Client/assets/styles/style.css").toExternalForm());
+      stage.setScene(scene);
+      stage.setScene(scene);
+      stage.show();
    }
 }
