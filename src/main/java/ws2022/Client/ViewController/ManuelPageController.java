f65c6c756df3f1209045fa7d05a32dfca4733843
Ferid Gökkaya <116716082+ferid99@users.noreply.github.com>
2023-01-22 16:13:25 +0100

    Create manualpage fxml

--- src/main/java/ws2022/Client/ViewController/ManuelPageController.java
+++ src/main/java/ws2022/Client/ViewController/ManuelPageController.java
@@ -3,15 +3,21 @@ package ws2022.Client.ViewController;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class ManuelPageController {
+   private Stage stage;
+   private Scene scene;
+   private Parent root;
+   Stage mystage;
+  
    SceneController sc = SceneController.getInstance();
    public void returnHome(ActionEvent event) throws IOException{
     sc.homeScreen(event);
    }
    
-   public void manualPage2(ActionEvent event) throws IOException {
-      sc.gotomanualpage(event);
-  }
-
+ 
 }
