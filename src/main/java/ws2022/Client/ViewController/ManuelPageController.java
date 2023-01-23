ff764b09fb93e2ffe59d0e44f3ffea36ab09fe5c
Ferid Gökkaya <116716082+ferid99@users.noreply.github.com>
2023-01-24 00:26:55 +0100

    complete Manual page 2

--- src/main/java/ws2022/Client/ViewController/ManuelPageController.java
+++ src/main/java/ws2022/Client/ViewController/ManuelPageController.java
@@ -7,19 +7,105 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import javafx.scene.Node;
 public class ManuelPageController {
-   private Stage stage;
-   private Scene scene;
-   private Parent root;
-   Stage mystage;
+   //private Stage stage;
+    Scene scene;
+    Parent root;
+   Stage stage;
   
    SceneController sc = SceneController.getInstance();
    public void returnHome(ActionEvent event) throws IOException{
     sc.homeScreen(event);
    }
    public void switchtomanualpage2(ActionEvent event)throws IOException{
-       
+      Parent root = FXMLLoader.load(getClass().getResource("ws2022/fxml/ManualpagePane2.fxml"));
+      stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+      scene = new Scene(root);
+      scene.getStylesheets()
+            .add(getClass().getResource("/ws2022/Client/assets/styles/style.css").toExternalForm());
+      stage.setScene(scene);
+      stage.setScene(scene);
+      stage.show();
+      // sc.gotomanualpage(event);
+    
    }
+   public void switchtomanualpage3(ActionEvent event) throws IOException {
+     
+      Parent root = FXMLLoader.load(getClass().getResource("ws2022/fxml/ManPage3.fxml"));
+      stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+      scene = new Scene(root);
+      scene.getStylesheets()
+            .add(getClass().getResource("/ws2022/Client/assets/styles/style.css").toExternalForm());
+      stage.setScene(scene);
+      stage.setScene(scene);
+      stage.show();
+      // sc.gotomanualpage(event);
  
+  }
+  public void switchtomanualpage4(ActionEvent event) throws IOException {
+     
+   Parent root = FXMLLoader.load(getClass().getResource("ws2022/fxml/ManualPage4.fxml"));
+   stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+   scene = new Scene(root);
+   scene.getStylesheets()
+         .add(getClass().getResource("/ws2022/Client/assets/styles/style.css").toExternalForm());
+   stage.setScene(scene);
+   stage.setScene(scene);
+   stage.show();
+   // sc.gotomanualpage(event);
+
+}
+public void switchtomanualpage5(ActionEvent event) throws IOException {
+     
+   Parent root = FXMLLoader.load(getClass().getResource("ws2022/fxml/test.fxml"));
+   stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+   scene = new Scene(root);
+   scene.getStylesheets()
+         .add(getClass().getResource("/ws2022/Client/assets/styles/style.css").toExternalForm());
+   stage.setScene(scene);
+   stage.setScene(scene);
+   stage.show();
+   // sc.gotomanualpage(event);
+
+}
+public void switchtomanualpage6(ActionEvent event) throws IOException {
+     
+   Parent root = FXMLLoader.load(getClass().getResource("ws2022/fxml/ManualPage6.fxml"));
+   stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+   scene = new Scene(root);
+   scene.getStylesheets()
+         .add(getClass().getResource("/ws2022/Client/assets/styles/style.css").toExternalForm());
+   stage.setScene(scene);
+   stage.setScene(scene);
+   stage.show();
+   // sc.gotomanualpage(event);
+
+}
+public void switchtomanualpage7(ActionEvent event) throws IOException {
+     
+   Parent root = FXMLLoader.load(getClass().getResource("ws2022/fxml/ManualPage7.fxml"));
+   stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+   scene = new Scene(root);
+   scene.getStylesheets()
+         .add(getClass().getResource("/ws2022/Client/assets/styles/style.css").toExternalForm());
+   stage.setScene(scene);
+   stage.setScene(scene);
+   stage.show();
+   // sc.gotomanualpage(event);
+
+}
+public void switchtomanualpage8(ActionEvent event) throws IOException {
+     
+   Parent root = FXMLLoader.load(getClass().getResource("ws2022/fxml/ManualPage8.fxml"));
+   stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+   scene = new Scene(root);
+   scene.getStylesheets()
+         .add(getClass().getResource("/ws2022/Client/assets/styles/style.css").toExternalForm());
+   stage.setScene(scene);
+   stage.setScene(scene);
+   stage.show();
+   // sc.gotomanualpage(event);
+
+}
 }
