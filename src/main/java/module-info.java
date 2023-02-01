64f9bb46551399f9e720abd1436717edefb25dd7
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2023-02-01 01:28:30 +0100

    configurate for building jar file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -12,7 +12,6 @@ module ws2022 {
     exports ws2022.Client.Model;
     exports ws2022.Client.RunScreen;
     exports ws2022.Client.ViewController;
-    // exports ws2022.Client.Logic;
     exports ws2022;
 
 }
---
+++ src/main/java/ws2022/Starter.java
@@ -0,0 +1,7 @@
+package ws2022;
+
+public class Starter {
+    public static void main(final String[] args) {
+        App.main(args);
+    }
+}
