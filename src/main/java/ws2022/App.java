2b005c0908afdcde45125ad36186be6e26709f6b
Ferid Gökkaya <116716082+ferid99@users.noreply.github.com>
2023-02-04 00:05:26 +0100

    Update App.java

--- src/main/java/ws2022/App.java
+++ src/main/java/ws2022/App.java
@@ -19,13 +19,14 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/ws2022/fxml/HomeScreen.fxml"));
-        System.out.println("START!");
+        System.out.println("Starting...");
         scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("Nanu");
         GameManager.stage = stage;
         stage.setScene(scene);
         stage.show();
+        
     }
 
     public static void main(String[] args) {
